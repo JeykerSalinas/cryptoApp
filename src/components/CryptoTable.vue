a9c412661b96a46200a265587befb820591c5e28
JeykerSalinas <jeyker.salinas13@gmail.com>
2022-04-12 16:34:20 -0400

    Rendering Cryptos Table with  pagination

--- src/components/CryptoTable.vue
+++ src/components/CryptoTable.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>Im a cryptotable</div>
+  <div>
+    <div>
+      <b-table
+        id="my-table"
+        :items="items"
+        :per-page="perPage"
+        :current-page="currentPage"
+        small
+      >
+        <template #cell(id)="data">
+          <router-link :to="'./about/' + data.item.id">{{
+            data.item.id
+          }}</router-link>
+        </template>
+      </b-table>
+    </div>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
+  </div>
 </template>
 
 <script>
 export default {
   name: "CryptoTable",
+  data() {
+    return {
+      perPage: 15,
+      currentPage: 1,
+    };
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="home">
-    <CryptoTable />
+    <CryptoTable :items="cryptos" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import CryptoTable from "@/components/CryptoTable.vue";
+import axios from "axios";
 
 export default {
   name: "HomeView",
   components: {
     CryptoTable,
+  },
+  data() {
+    return {
+      cryptos: [],
+    };
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      try {
+        const request = await axios.get("https://api.coinpaprika.com/v1/coins");
+        const data = request.data;
+        this.cryptos = data;
+        console.log(this.cryptos);
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
