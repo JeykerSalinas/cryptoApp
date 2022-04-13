33370f6c182e4d77e7b80648c75741277a4412cc
JeykerSalinas <jeyker.salinas13@gmail.com>
2022-04-13 11:24:06 -0400

    Adding a small search filter

--- src/components/CryptoTable.vue
+++ src/components/CryptoTable.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
+    <div></div>
+    <div class="w-25">
+      <input
+        type="text"
+        class="form form-control"
+        @keyup="filterData()"
+        v-model="search"
+        placeholder="Search Crypto"
+      />
+    </div>
     <div>
       <b-table
+        hover
+        responsive
         id="my-table"
-        :items="items"
+        :items="!search ? items : filteredItems"
+        :fields="fields"
         :per-page="perPage"
         :current-page="currentPage"
         small
       >
-        <template #cell(id)="data">
+        <!-- <template #cell(id)="data">
           <router-link :to="'./crypto/' + data.item.id">{{
             data.item.id
           }}</router-link>
-        </template>
+        </template> -->
       </b-table>
     </div>
     <b-pagination
@@ -32,11 +45,30 @@ export default {
     return {
       perPage: 15,
       currentPage: 1,
+      fields: [
+        "name",
+        "symbol",
+        "rank",
+        { key: "is_new", label: "New" },
+        { key: "is_active", label: "Active" },
+        "type",
+      ],
+      filteredItems: [],
+      search: "",
     };
   },
   computed: {
     rows() {
       return this.items.length;
+    },
+  },
+  methods: {
+    filterData() {
+      this.filteredItems = this.items.filter(
+        (item) =>
+          item.name.toLowerCase().includes(this.search.toLowerCase().trim()) ||
+          item.symbol.toLowerCase().includes(this.search.toLowerCase().trim())
+      );
     },
   },
   props: {
